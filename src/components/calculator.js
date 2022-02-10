6337818715548db8dfd59010b313687218b45a99
joskalenda <joskalenda3@gmail.com>
2022-02-10 09:45:54 +0200

    Creat handler function

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import calculate from '../logic/calculate';
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: 0,
+      next: '',
+      operation: '',
+    };
   }
 
-  render() {
-    const CalcElement = ['AC', '%/+', '%', '+', 7, 8, 9, '*', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '=']
-      .map((item) => <button type="button" className="button" key={item} id={item}>{item}</button>);
+  HandleClick = (element) => this.setState((e) => calculate(e, element.target.textContent));
 
+  render() {
+    const { total, next, operation } = this.state;
+    const CalcElement = ['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '=']
+      .map((item) => <button type="button" onClick={this.HandleClick} className="button" key={item} id={item}>{item}</button>);
     return (
       <div className="container">
         <div className="display--section">{0}</div>
