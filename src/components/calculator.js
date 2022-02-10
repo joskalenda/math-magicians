5cc1622d14c00fd92fef419d35237ecf2fe64a6e
joskalenda <joskalenda3@gmail.com>
2022-02-10 10:09:11 +0200

    Final touches

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -4,20 +4,16 @@ import calculate from '../logic/calculate';
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      total: 0,
-      next: '',
-      operation: '',
-    };
+    this.state = { total: 0, next: '', operation: '' };
   }
 
   HandleClick = (element) => this.setState((e) => calculate(e, element.target.textContent));
 
   render() {
     const { total, next, operation } = this.state;
+    const doMath = `${total || operation || next ? `${total || ''} ${operation || ''} ${next || ''}` : '0'} `;
     const CalcElement = ['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '=']
       .map((item) => <button type="button" onClick={this.HandleClick} className="button" key={item} id={item}>{item}</button>);
-    const doMath = `${total || operation || next ? `${total || ''} ${operation || ''} ${next || ''}` : '0'} `;
     return (
       <div className="container">
         <div className="display--section">{doMath}</div>
