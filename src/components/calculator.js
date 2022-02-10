17a1a972142509e3ada5904c61db36ec9c678457
joskalenda <joskalenda3@gmail.com>
2022-02-10 09:46:27 +0200

    Display calculations

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -17,9 +17,10 @@ class Calculator extends Component {
     const { total, next, operation } = this.state;
     const CalcElement = ['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '=']
       .map((item) => <button type="button" onClick={this.HandleClick} className="button" key={item} id={item}>{item}</button>);
+    const doMath = `${total || operation || next ? `${total || ''} ${operation || ''} ${next || ''}` : '0'} `;
     return (
       <div className="container">
-        <div className="display--section">{0}</div>
+        <div className="display--section">{doMath}</div>
         <div className="buttons--ssection">{CalcElement}</div>
       </div>
     );
